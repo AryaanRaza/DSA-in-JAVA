227846a5f1827cb08d017c390400066d613eb990
Md. Aryaan Raza <254004041+AryaanRaza@users.noreply.github.com>
2026-02-14 16:12:57 +0530

    Added insert at end

--- src/linkedlist/DoublyLinkedList.java
+++ src/linkedlist/DoublyLinkedList.java
@@ -19,10 +19,43 @@ public class DoublyLinkedList
 
     private Node head;
 
+    public void insertAtEnd(int data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
 
+    public void traverse()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        };
+        System.out.print("null");
+    }
 
     public static void main(String[] args)
     {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertAtEnd(5);
+        list.insertAtEnd(10);
+        list.insertAtEnd(15);
+        list.insertAtEnd(20);
+        list.insertAtEnd(25);
 
+        list.traverse();
     }
 }
