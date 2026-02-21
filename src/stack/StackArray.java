d56d7d41684713bdcf77a6b1f47883cb903878ef
Md. Aryaan Raza <254004041+AryaanRaza@users.noreply.github.com>
2026-02-21 19:58:01 +0530

    Added push operation

--- src/stack/StackArray.java
+++ src/stack/StackArray.java
@@ -2,13 +2,24 @@ package stack;
 
 public class StackArray
 {
-    private int[] arr;
-    private int top;
-    private int capacity;
+    private int[] arr;         // array to store stack elements
+    private int top;           // index of the top element
+    private int capacity;      // maximum size of stack
 
+    //Constructor
     public StackArray(int size){
         capacity=size;
         arr = new int[capacity];
         top=-1;
     }
+
+    //Push Operation
+    public void push(int x){
+        if(top == capacity-1){
+            System.out.print("\nStack Overflow , Cannot Push "+ x);
+            return;
+        }
+        arr[++top]=x;
+    }
+
 }
