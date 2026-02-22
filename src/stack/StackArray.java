4c4997adfaef0d545f0f30a19d8b5bfceda2f305
Md. Aryaan Raza <254004041+AryaanRaza@users.noreply.github.com>
2026-02-22 15:34:37 +0530

    feat: add peek and isEmpty methods

--- src/stack/StackArray.java
+++ src/stack/StackArray.java
@@ -7,29 +7,51 @@ public class StackArray
     private int capacity;      // maximum size of stack
 
     //Constructor
-    public StackArray(int size){
-        capacity=size;
+    public StackArray(int size)
+    {
+        capacity = size;
         arr = new int[capacity];
-        top=-1;
+        top = -1;
     }
 
     //Push Operation
-    public void push(int x){
-        if(top == capacity-1){
-            System.out.print("\nStack Overflow , Cannot Push "+ x);
+    public void push(int x)
+    {
+        if (top == capacity - 1)
+        {
+            System.out.print("\nStack Overflow , Cannot Push " + x);
             return;
         }
-        arr[++top]=x;
+        arr[++top] = x;
     }
 
     //Pop Operation
-    public int pop(){
-        if(top==-1){
+    public int pop()
+    {
+        if (top == -1)
+        {
             System.out.print("\nStack Underflow! Cannot pop");
             return -1;
         }
 
         return arr[top--];
+    }
+
+    //Peek operation
+    public int peek()
+    {
+        if (top == -1)
+        {
+            System.out.print("\nStack is empty");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    //Check if stack is empty
+    public boolean isEmpty()
+    {
+        return top == -1;
     }
 
 
