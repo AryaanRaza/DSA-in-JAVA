0132b8b899bf866db9711a45863cf860a5fd5f3d
Md. Aryaan Raza <254004041+AryaanRaza@users.noreply.github.com>
2026-02-26 00:12:38 +0530

    Added menu

--- src/stack/StackLinkedList.java
+++ src/stack/StackLinkedList.java
@@ -2,6 +2,8 @@ package stack;
 
 import linkedlist.SinglyLinkedList;
 
+import java.util.Scanner;
+
 public class StackLinkedList
 {
     private static class Node
@@ -75,13 +77,40 @@ public class StackLinkedList
     }
 
 
-    public int getSize() {
+    public int getSize()
+    {
         return size;
     }
 
     public static void main(String[] args)
     {
-        StackLinkedList s = new StackLinkedList();
+        Scanner sc = new Scanner(System.in);
+        StackLinkedList stack = new StackLinkedList();
+
+        boolean exit = false;
+        while (!exit)
+        {
+            System.out.println("\n1.Push 2.Pop 3.Peek 4.Display 5.Size 6.Exit");
+            System.out.print("Choose operation: ");
+            int choice = sc.nextInt();
+
+            switch (choice)
+            {
+                case 1 ->
+                {
+                    System.out.print("Enter element to push: ");
+                    int x = sc.nextInt();
+                    stack.push(x);
+                }
+                case 2 -> stack.pop();
+                case 3 -> System.out.println("Top element: " + stack.peek());
+                case 4 -> stack.display();
+                case 5 -> System.out.println("Stack size: " + stack.getSize());
+                case 6 -> exit = true;
+                default -> System.out.println("Invalid choice");
+            }
+        }
 
     }
 }
+
