3d8a9b7d8a693a76bdbc0a5e08911f07cd3a18f2
unknown <aryaan.raza.dev@gmail.com>
2026-03-02 00:17:52 +0530

    feat: add Balanced Parentheses problem using Stack

--- src/stack/BalancedParentheses.java
+++ src/stack/BalancedParentheses.java
@@ -5,19 +5,24 @@ import java.util.*;
 public class BalancedParentheses
 {
 
-    public static boolean isBalanced(String str){
+    public static boolean isBalanced(String str)
+    {
         Stack<Character> stack = new Stack<>();
-        for(char ch : str.toCharArray()){
+        for (char ch : str.toCharArray())
+        {
             // If opening bracket, push
-            if (ch == '(' || ch == '{' || ch == '[') {
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
                 stack.push(ch);
             }
 
             // If closing bracket
-            else if (ch == ')' || ch == '}' || ch == ']') {
+            else if (ch == ')' || ch == '}' || ch == ']')
+            {
 
                 // If stack empty → no matching opening
-                if (stack.isEmpty()) {
+                if (stack.isEmpty())
+                {
                     return false;
                 }
 
@@ -25,8 +30,9 @@ public class BalancedParentheses
 
                 // Check matching pair
                 if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '['))
+                {
                     return false;
                 }
             }
@@ -37,6 +43,18 @@ public class BalancedParentheses
 
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter expression : ");
+        String input = sc.nextLine();
+
+        if (isBalanced(input))
+        {
+            System.out.println("Balanced Parentheses ✅");
+        }
+        else
+        {
+            System.out.println("Not Balanced ❌");
+        }
 
     }
 
