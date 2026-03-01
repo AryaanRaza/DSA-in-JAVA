f08df3f7945a20fc9e7fe8ab24bf9756e48bd478
unknown <aryaan.raza.dev@gmail.com>
2026-03-02 00:07:11 +0530

    Added the method to check if string is balanced

--- src/stack/BalancedParentheses.java
+++ src/stack/BalancedParentheses.java
@@ -1,7 +1,38 @@
 package stack;
 
+import java.util.*;
+
 public class BalancedParentheses
 {
 
+    public static boolean isBalanced(String str){
+        Stack<Character> stack = new Stack<>();
+        for(char ch : str.toCharArray()){
+            // If opening bracket, push
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // If closing bracket
+            else if (ch == ')' || ch == '}' || ch == ']') {
+
+                // If stack empty → no matching opening
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                // Check matching pair
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
 
 }
